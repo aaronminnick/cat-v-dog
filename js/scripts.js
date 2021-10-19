7e6a5327b431e6eaaad4f21075c1077a070d7fe0
Aaron Minnick <abminnick@gmail.com>
2021-10-19 15:42:41 -0700

    hide li beyond the most recent, add button to manually show history
    Co-authored-by: Albert Lee <leesga8@gmail.com>
    Co-authored-by: Christopher Neal <christopher.m.neal@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,27 @@ $(document).ready(function() {
     event.preventDefault();
     const meow = $("input#catsay").val();
     
-    $("ul", "#cat-col").append("<li>"+ meow +"</li>");
-    $("ul", "#dog-col").append("<li>Woof!</li>");
+    $("ul", "#cat-col").prepend("<li>"+ meow +"</li>");
+    $("ul", "#dog-col").prepend("<li>" + meow +" you say? Woof!</li>");
     $("#cat-form").trigger("reset");
+    $("li").hide();
+    $("ul", "#cat-col").children("li").slice(0, 1).show();
+    $("ul", "#dog-col").children("li").slice(0, 1).show();
   });
 
   $("#dog-form").submit(function(event) {
     event.preventDefault();
     const woof = $("input#dogsay").val();
     
-    $("ul", "#dog-col").append("<li>"+ woof +"</li>");
-    $("ul", "#cat-col").append("<li>...Meow!</li>");
+    $("ul", "#dog-col").prepend("<li>"+ woof +"</li>");
+    $("ul", "#cat-col").prepend("<li>" + woof + " you say? ...Meow!</li>");
     $("#dog-form").trigger("reset");
+    $("li").hide();
+    $("ul", "#cat-col").children("li").slice(0, 1).show();
+    $("ul", "#dog-col").children("li").slice(0, 1).show();
+  });
+
+  $("#history").click(function() {
+    $("li").show();
   });
 });
